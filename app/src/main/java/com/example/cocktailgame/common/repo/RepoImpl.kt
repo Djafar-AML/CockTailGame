587f7c6097b8a93e7939d3be40e32cafa29c5be5
Jeff <jafaragha77@gmail.com>
2022-09-18 14:03:10 +0430

    - green of TDD

--- app/src/main/java/com/example/cocktailgame/common/repo/RepoImpl.kt
+++ app/src/main/java/com/example/cocktailgame/common/repo/RepoImpl.kt
@@ -11,9 +11,13 @@ class RepoImpl(
 ) : Repo {
 
     override fun saveHighScore(score: Int) {
-        val editor = sharedPrefs.edit()
-        editor.putInt(HIGH_SCORE_KEY, score)
-        editor.apply()
+
+        val highScore = getHighScore()
+        if (score > highScore) {
+            val editor = sharedPrefs.edit()
+            editor.putInt(HIGH_SCORE_KEY, score)
+            editor.apply()
+        }
     }
 
     override fun getHighScore() = sharedPrefs.getInt(HIGH_SCORE_KEY, 0)
